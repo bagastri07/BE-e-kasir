4d47a47afbecfd17ea85c8010c017f2215bb365d
bagastri07 <bagaskuhaku@gmail.com>
2022-01-13 23:04:36 +0700

    implement print and dashboard

--- Controllers/BasicController.js
+++ Controllers/BasicController.js
@@ -1,4 +1,6 @@
 const Product = require('../Models/ProductSchema')
+const Note = require('../Models/NoteSchema')
+const formatRupiah = require('../_helpers/formatRupiah')
 
 
 
@@ -10,12 +12,28 @@ const BasicController = {
         res.render('Pages/register')
     },
     dashboard: (req, res) => {
-        Product.countDocuments({user: req.user._id}, (err, countDoc) => {
+        Product.countDocuments({user: req.user._id}, async (err, countDoc) => {
             if (err) throw err
             
+
+            notes = await Note.find({user: req.user._id})
+
+            totalPendapatan = 0
+            notes.forEach(el => {
+                totalPendapatan = totalPendapatan + el.total
+            });
+
+            jumlahNota = await Note.countDocuments({user: req.user._id})
+
+            rataRata = totalPendapatan/jumlahNota
+
             res.render('Pages/dashboard', {
                 userName: req.user.name,
-                productCount: countDoc
+                productCount: countDoc,
+                totalPendapatan: totalPendapatan,
+                formatRupiah: formatRupiah,
+                jumlahPenjualan: jumlahNota,
+                rataRata : rataRata
             })
         })
         
